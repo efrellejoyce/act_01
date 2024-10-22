250a11936f1986cc87e1e0a4d264e06c2c992ff1
Efrelle Joyce Ducyao <efrellejoyced@gmail.com>
2024-10-22 07:54:46 +0700

    initial commit

---
+++ arrayActivity.js
@@ -0,0 +1,11 @@
+// Favorite Singers
+let favSingers = ["Ed Sheeran", "Moira Dela Torre", "Paolo Benjamin"];
+console.log("My first favorite singer is", favSinger[0]);
+
+//Favorite Numbers
+let favNumbers = ["4", "24", "10", "15"];
+let mixedArr = ["string", [otherarray], 123, true];
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
